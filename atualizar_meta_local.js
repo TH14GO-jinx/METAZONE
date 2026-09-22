ec6c8dde9167f7dbb0c9b2b6a9497aebfb0f848c
TH1G4 <souzathiago5694@gmail.com>
2026-09-22 12:31:56 -0300

    switch: codmunity.gg/pt tier-list (tabela funciona)

--- atualizar_meta_local.js
+++ atualizar_meta_local.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
-const URL_CODMUNITY = 'https://wzstats.gg/pt';
+const URL_CODMUNITY = 'https://codmunity.gg/pt/tier-list/warzone';
 
 const ARQUIVOS_JSON = [
     './armas_bo7.json',
