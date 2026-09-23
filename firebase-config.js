047db0a6ff20e4e418da7073ba7c87ffe29b42c2
th14go-jinx <souzathiago5694@gmail.com>
2026-09-23 15:49:12 -0300

    Refactor Firebase initialization to use modular SDK

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "apiKey": "AIzaSyCkR3L5oy2WkWKCtLwXvl8XrkstGpsV0ng",
-  "authDomain": "builds-4213e.firebaseapp.com",
-  "projectId": "builds-4213e",
-  "storageBucket": "builds-4213e.firebasestorage.app",
-  "messagingSenderId": "612728705593",
-  "appId": "1:612728705593:web:2091b8a995c1d04de273e8",
-  "measurementId": "G-9PCPQ90CKS"
+  apiKey: "AIzaSyCkR3L5oy2WkWKCtLwXvl8XrkstGpsV0ng",
+  authDomain: "builds-4213e.firebaseapp.com",
+  projectId: "builds-4213e",
+  storageBucket: "builds-4213e.firebasestorage.app",
+  messagingSenderId: "612728705593",
+  appId: "1:612728705593:web:2091b8a995c1d04de273e8",
+  measurementId: "G-9PCPQ90CKS"
 };
 
-if (typeof firebase !== 'undefined' && firebase.apps && !firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const db = (typeof firebase !== 'undefined' && firebase.firestore) ? firebase.firestore() : null;
-const auth = (typeof firebase !== 'undefined' && firebase.auth) ? firebase.auth() : null;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
